15dcfa0b9455d8c385812011b74dc63d3f41983d
Lars <lars.scheme@gmail.com>
2017-12-02 10:18:38 +0100

    Fullscreen window with D3D9 support

---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonutz/d3d9"
+	"github.com/gonutz/w32"
+	"github.com/gonutz/win"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"time"
+)
+
+func main() {
+	defer handlePanics()
+
+	runtime.LockOSThread()
+
+	win.HideConsoleWindow()
+
+	windowW, windowH := 800, 600
+
+	window, err := win.NewWindow(
+		0,
+		0,
+		windowW,
+		windowH,
+		"LD40window",
+		func(window w32.HWND, msg uint32, w, l uintptr) uintptr {
+			switch msg {
+			case w32.WM_KEYDOWN:
+				if w == w32.VK_ESCAPE {
+					win.CloseWindow(window)
+				}
+				return 0
+			case w32.WM_SIZE:
+				windowW, windowH = int((uint(l))&0xFFFF), int((uint(l)>>16)&0xFFFF)
+				return 0
+			case w32.WM_DESTROY:
+				w32.PostQuitMessage(0)
+				return 0
+			default:
+				return w32.DefWindowProc(window, msg, w, l)
+			}
+		},
+	)
+	check(err)
+	win.EnableFullscreen(window)
+
+	d3d, err := d3d9.Create(d3d9.SDK_VERSION)
+	check(err)
+	defer d3d.Release()
+
+	var createFlags uint32 = d3d9.CREATE_SOFTWARE_VERTEXPROCESSING
+	caps, err := d3d.GetDeviceCaps(d3d9.ADAPTER_DEFAULT, d3d9.DEVTYPE_HAL)
+	if err == nil &&
+		caps.DevCaps&d3d9.DEVCAPS_HWTRANSFORMANDLIGHT != 0 {
+		createFlags = d3d9.CREATE_HARDWARE_VERTEXPROCESSING
+	}
+	presentParameters := d3d9.PRESENT_PARAMETERS{
+		Windowed:         1,
+		HDeviceWindow:    d3d9.HWND(window),
+		SwapEffect:       d3d9.SWAPEFFECT_DISCARD,
+		BackBufferWidth:  uint32(windowW),
+		BackBufferHeight: uint32(windowH),
+		BackBufferFormat: d3d9.FMT_A8R8G8B8,
+		BackBufferCount:  1,
+	}
+	device, actualPP, err := d3d.CreateDevice(
+		d3d9.ADAPTER_DEFAULT,
+		d3d9.DEVTYPE_HAL,
+		d3d9.HWND(window),
+		createFlags,
+		presentParameters,
+	)
+	presentParameters = actualPP
+	check(err)
+	defer device.Release()
+
+	setRenderState := func(device *d3d9.Device) {
+		device.SetRenderState(d3d9.RS_CULLMODE, d3d9.CULL_NONE)
+		device.SetRenderState(d3d9.RS_SRCBLEND, d3d9.BLEND_SRCALPHA)
+		device.SetRenderState(d3d9.RS_DESTBLEND, d3d9.BLEND_INVSRCALPHA)
+		device.SetRenderState(d3d9.RS_ALPHABLENDENABLE, 1)
+	}
+	setRenderState(device)
+
+	deviceIsLost := false
+	const frameDelay = time.Second / 60
+	lastFrame := time.Now().Add(-frameDelay)
+	win.RunMainGameLoop(func() {
+		now := time.Now()
+		if now.Sub(lastFrame) < frameDelay {
+			time.Sleep(time.Nanosecond)
+		} else {
+			lastFrame = now
+
+			if deviceIsLost {
+				_, err = device.Reset(presentParameters)
+				if err == nil {
+					deviceIsLost = false
+					setRenderState(device)
+					// TODO reset vertex buffers
+					// TODO reset vertex declarations
+					// TODO reset textures
+				}
+			} else {
+				device.Clear(nil, d3d9.CLEAR_TARGET, d3d9.ColorRGB(255, 0, 0), 1, 0)
+				presentErr := device.Present(
+					&d3d9.RECT{0, 0, int32(windowW), int32(windowH)},
+					nil, 0, nil,
+				)
+				if presentErr != nil {
+					if presentErr.Code() == d3d9.ERR_DEVICELOST {
+						deviceIsLost = true
+					} else {
+						check(presentErr)
+					}
+				}
+			}
+		}
+	})
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// handle all panics in the program and open a log file after a crash
+func handlePanics() {
+	if err := recover(); err != nil {
+		// in case of a panic, create a message with the current stack
+		msg := fmt.Sprintf("panic: %v\nstack:\n\n%s\n", err, debug.Stack())
+
+		// print it to stdout
+		fmt.Println(msg)
+
+		// write it to a log file
+		filename := filepath.Join(
+			os.Getenv("APPDATA"),
+			"ide_log_"+time.Now().Format("2006_01_02__15_04_05")+".txt",
+		)
+		ioutil.WriteFile(filename, []byte(msg), 0777)
+
+		// open the log file with the default text viewer
+		exec.Command("cmd", "/C", filename).Start()
+
+		// pop up a message box
+		w32.MessageBox(
+			0,
+			msg,
+			"The program crashed",
+			w32.MB_OK|w32.MB_ICONERROR|w32.MB_TOPMOST,
+		)
+	}
+}
