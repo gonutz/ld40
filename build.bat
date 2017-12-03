4ff5c53a4ce42cafce130d5ccdd68c987d06a491
Lars <lars.scheme@gmail.com>
2017-12-03 23:43:38 +0100

    Go get payload tool in build script

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@ call build_shaders.bat
 set GOARCH=386
 go build -ldflags "-s -w -H=windowsgui" -o ld40.exe
 call build_data_blob.bat
+go get github.com/gonutz/payload/cmd/payload
 payload -data=data.blob -exe=ld40.exe
