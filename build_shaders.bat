8efc1b0df445ccef662dbba1f67ffbb95d23f2d2
Lars <lars.scheme@gmail.com>
2017-12-05 16:24:05 +0100

    Make shader build script less noisy

--- build_shaders.bat
+++ build_shaders.bat
@@ -1,7 +1,10 @@
+@echo off
 REM compile shaders *.vs and *.ps into object code files *.vso and *.pso
 set FXCOMPILER=fxc.exe
-for /r %%f in (*.vs) do %FXCOMPILER% /WX /T vs_2_0 /Fo %%~nf.vso %%~nf.vs
-for /r %%f in (*.ps) do %FXCOMPILER% /WX /T ps_2_0 /Fo %%~nf.pso %%~nf.ps
+REM print std out of the shader compiler to a file so only error messages from stderr appear in cmd
+for /r %%f in (*.vs) do %FXCOMPILER% /WX /T vs_2_0 /Fo %%~nf.vso %%~nf.vs > temp
+for /r %%f in (*.ps) do %FXCOMPILER% /WX /T ps_2_0 /Fo %%~nf.pso %%~nf.ps > temp
+del temp
 
 REM convert the object code files into Go code files
 go get github.com/gonutz/bin2go/v2/bin2go
