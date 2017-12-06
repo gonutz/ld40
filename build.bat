1b536976df92537ded4f3e16294a8e7b16b1ebb6
gonutz <lars.scheme@gmail.com>
2017-12-06 23:33:07 +0100

    Reset GOARCH in build so tools are installed to GOPATH\bin
    Otherwise they might get installed to GOPATH\bin\windows_386 which is not in the PATH.

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ rsrc -arch amd64 -ico icon.ico -o rsrc_amd64.syso
 
 set GOARCH=386
 go build -ldflags "-s -w -H=windowsgui" -o ld40.exe
+set GOARCH=
 
 call build_data_blob.bat
 go get github.com/gonutz/payload/cmd/payload
